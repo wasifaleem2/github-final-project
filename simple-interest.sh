2fdb0bea915eac38acbae90134545e389bdd7d11
Wasif Aleem <60383635+wasifaleem2@users.noreply.github.com>
2023-08-19 23:07:27 +0500

    Update simple-interest.sh add username

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # wasifaleem2
+   # wasifaleem2 
    # Input:
    # p, principal amount
    # t, time period in years
